1c29d96e72f2a196f6fbb0165f87844ac5f66d60
biswarup445 <biswarup@distronix.in>
2025-07-04 19:35:33 +0530

    add camelize function.

--- index.js
+++ index.js
@@ -34,5 +34,13 @@ function capitalize(str) {
   return arr.join(" ");
 };
 
+// camel Case convertion function.
+function camelize(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+    return index === 0 ? word.toLowerCase() : word.toUpperCase();
+  }).replace(/\s+/g, '');
+}
 
-module.exports = { helloNpm, sleep, randomInt, randomFloat, validateEmail, capitalize };
+
+
+module.exports = { helloNpm, sleep, randomInt, randomFloat, validateEmail, capitalize, camelize };
